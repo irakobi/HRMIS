778e474fe5040620866defe4d59c65b98fa32fc8
yvento <yventobusiness24@gmail.com>
2023-10-25 21:35:38 +0200

    Updated salary calculations

--- src/salary.py
+++ src/salary.py
@@ -51,31 +51,27 @@ class Salary:
         """
         # Check the type of employee
         if employee_object.__class__.__name__ == 'Employee':
-            print(f"Regular employee salary information"
+            print(f"Regular employee salary information\n"
                   f'-----------------------------------\n'
                   f'Base salary: {employee_object.get_salary()/12}\n'
-                  f'Net pay: {(employee_object.calculate_earnings()/12) * self.tax_rate}')
+                  f'Net pay: {self.calculate_monthly_salary(employee_object)}')
         elif employee_object.__class__.__name__ == 'Manager':
-            overtime_amount = (employee_object.get_overtime_pay_per_hour() *
-                               employee_object.get_overtime_hours())
             allowance_amount = employee_object.get_salary() * (employee_object.get_allowance_rate() / 100)
-            print(f"Manager salary information"
+            print(f"Manager salary information\n"
                   f'--------------------------\n'
                   f'Base salary: {employee_object.get_salary()/12}\n'
                   f'Allowance amount: {allowance_amount/12}\n'
-                  f'Overtime amount: {overtime_amount/12}\n'
-                  f'Net pay: {(employee_object.calculate_earnings()/12) * self.tax_rate}')
+                  f'Overtime amount: {employee_object.overtime()/12}\n'
+                  f'Net pay: {self.calculate_monthly_salary(employee_object)}')
         elif employee_object.__class__.__name__ == 'Director':
-            overtime_amount = (employee_object.get_overtime_pay_per_hour() *
-                               employee_object.get_overtime_hours())
-            print(f"Director salary information"
+            print(f"Director salary information\n"
                   f'---------------------------\n'
                   f'Base salary: {employee_object.get_salary()/12}\n'
                   f'Bonus: {employee_object.get_annual_bonus()/12}\n'
-                  f'Overtime amount: {overtime_amount/12}\n'
-                  f'Net pay: {(employee_object.calculate_earnings()/12) * self.tax_rate}')
+                  f'Overtime amount: {employee_object.overtime()/12}\n'
+                  f'Net pay: {self.calculate_monthly_salary(employee_object)}')
         elif employee_object.__class__.__name__ == 'Intern':
-            print(f"Intern salary information"
+            print(f"Intern salary information\n"
                   f'-------------------------\n'
                   f'Base salary: {employee_object.get_salary()/12}\n'
-                  f'Net pay: {(employee_object.calculate_earnings()/12) * self.tax_rate}')
+                  f'Net pay: {self.calculate_monthly_salary(employee_object)}')
